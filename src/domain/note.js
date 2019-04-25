f5a7dfdf2c588022082b95fe380f067eb08bd178
Marcelino Madriaga <madriaga.marcelino.jr@gmail.cpm>
2019-04-25 11:20:51 +0800

    added version domain

--- src/domain/note.js
+++ src/domain/note.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const _ = require('lodash');
+const domain = require('../domain');
 
 class Note {
     constructor(note) {
@@ -11,23 +12,49 @@ class Note {
         return this._note.id;
     }
 
+    get version() {
+        return this._note.version;
+    }
+
     expose() {
         return _.pick(this._note, [
             'id',
             'subject',
             'body',
+            'version',
             'updatedAt',
         ]);
     }
 
     async update(body) {
+        await this.createVersion(this._note.subject, body, this._note.version);
         await this._note.update({
             body,
+            version: this._note.version + 1
         });
     }
 
     async delete() {
         await this._note.destroy();
+    }
+
+    async versions() {
+        const versions = await this._note.getVersions({
+            order: [
+                ['version', 'DESC']
+            ],
+        });
+        return _.map(versions, version => new domain.Version(version));
+    }
+
+    async createVersion(subject, body, prevVersion) {
+        const version = await this._note.createVersion({
+            subject,
+            body,
+            version: prevVersion
+        });
+
+        return new domain.Version(version);
     }
 }
 
